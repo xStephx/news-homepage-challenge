c27e2d00567e4bdf4fd2757d331f328c080db08d
xStephx <bojkovskistefan9@gmail.com>
2025-02-14 13:42:50 +0100

    added other comments for script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,12 @@ const menuIcon = document.getElementById('menu-icon');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeIcon = document.getElementById('close-icon');
 
+// Eventlistener for open menu
 menuIcon.addEventListener('click', () => {
   mobileMenu.classList.remove('hidden');
 });
 
+// Eventlistener for close menu
 closeIcon.addEventListener('click', () => {
   mobileMenu.classList.add('hidden');
 });
